b54840db3b38e435f2f9740ecc6e29fbf883610d
edigreat <heriberto.galdamez@gmail.com>
2013-11-30 09:51:37 -0600

    Configuracion de log

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -83,6 +83,9 @@ grails.exceptionresolver.params.exclude = ['password']
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
+//Directorio donde se pone los logs
+def logDirectory='target/'
+
 environments {
     development {
         grails.logging.jul.usebridge = true
@@ -90,26 +93,40 @@ environments {
     production {
         grails.logging.jul.usebridge = false
         // TODO: grails.serverURL = "http://www.changeme.com"
+        logDirectory='logs/'
     }
 }
 
-// log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+ 
+  appenders {
+    //console name: 'stdout', layout: pattern(conversionPattern: '%d{ISO8601}\t%p\t%c:%L\t%m%n'), threshold: org.apache.log4j.Level.ERROR
+    appender new org.apache.log4j.DailyRollingFileAppender(name: 'file', file: logDirectory + "trackbox.log", datePattern: '\'_\'yyyy-MM-dd', layout: pattern(conversionPattern: '%d{ISO8601}\t%p\t%c:%L\t%m%n'))
+  }
+ 
+  root {
+    debug 'file'
+  }
+ 
+  debug 'grails.app.controllers.com.makingdevs',
+      'grails.app.taglib.com.makingdevs',
+      'grails.app.services.com.makingdevs',
+      'grails.app.domain.com.makingdevs',
+      'grails.app.conf'
+ 
+  error 'org.codehaus.groovy.grails.web.servlet',      
+        'org.codehaus.groovy.grails.web.pages',        
+        'org.codehaus.groovy.grails.web.sitemesh',     
+        'org.codehaus.groovy.grails.web.mapping.filter',
+        'org.codehaus.groovy.grails.web.mapping',      
+        'org.codehaus.groovy.grails.commons',          
+        'org.codehaus.groovy.grails.plugins',          
+        'org.codehaus.groovy.grails.orm.hibernate',    
+        'org.springframework',
+        'org.hibernate',
+        'net.sf.ehcache.hibernate',
+        'grails.plugin.cache',
+        'grails.util',
+        'org.apache',
+        'org.grails.plugin'
 }
--- grails-app/controllers/com/makingdevs/HomeController.groovy
+++ grails-app/controllers/com/makingdevs/HomeController.groovy
@@ -3,7 +3,34 @@ package com.makingdevs
 class HomeController {
 
     def index() {
-
+    	log.debug "Hola mundo en el home controller"
     	render "Hola Mundo"
      }
+
+     def explorarRequest(){
+		log.debug	actionName
+		log.debug	actionUri
+		log.debug	controllerName
+		log.debug	controllerUri
+		log.debug	flash
+		log.debug	log
+		log.debug	params
+		log.debug	request
+		log.debug	response
+		log.debug	session
+		log.debug	servletContext
+
+		render "Atributos del request"
+     }
+
+    def accion1(){
+    log.debug actionName
+  	flash.variable = "Estare disponible en el siguiente request"
+  	redirect action:"accion2" // Generando un nuevo request
+	}
+ 
+	def accion2(){
+		log.debug actionName
+  	log.debug flash.variable // "Estaré disponible en el siguiente request"
+	}
 }
